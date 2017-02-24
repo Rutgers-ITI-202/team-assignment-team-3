450c4e994c36247a6ec6817c78e94b088ad1659f
LaurenRichardson <lnr38@scarletmail.rutgers.edu>
2017-02-24 00:07:14 -0500

    Start of Game
    Basic commands at beginning

--- src/adventure/Room.java
+++ src/adventure/Room.java
@@ -2,12 +2,13 @@ package adventure;
 
 public class Room {
 
-	private Room Room1 (String x)
+	private Room Room1 (String x) {
+		String roomDescription = "You look around an empty room. There are not windows or doors, you are trapped here forever.";
 		
-		public Description
-			System.out.println("You look around an empty room. There are not windows or doors, you are trapped here forever.");
+		String description(){
+			System.out.println(roomDescription);
 		
-
+	}
 		
 	}
 
--- src/adventure/TextAdventure.java
+++ src/adventure/TextAdventure.java
@@ -12,7 +12,18 @@ public class TextAdventure {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
- 
+			
+		Scanner mainscan = new Scanner(System.in);
+		
+		System.out.println("Hello, welcome to our game! Would you like to play?: ");
+		String response = mainscan.next();
+		
+		if (response.equalsIgnoreCase("Yes")){
+			AdventureModel.go;
+			}
+		else {
+			System.out.println("Ok bye");
+		}
 		
 	}
 
