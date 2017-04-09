1dae21262008cd56e83007600aca4c1646b608bc
LaurenRichardson <lnr38@scarletmail.rutgers.edu>
2017-04-09 14:04:57 -0400

    added stuff
    doesn’t work yet

--- src/adventure/Room.java
+++ src/adventure/Room.java
@@ -1,25 +1,46 @@
 package adventure;
 
 public class Room {
-	String name;
-	String roomDescription;
-	Room leftRoom;
-	Room rightRoom;
-	Item item;
-	
-	
-	
-	
-	
-	
-	private Room 1 {
-		String roomDescription = "You look around an empty room. There are not windows or doors, you are trapped here forever.";
-		leftRoom;
-		rightRoom;
+		public static String name = Room.setName();
+		public String roomDescription;
+		public Item item;
+		public arrayList connectingRooms
 		
+		public static String returnName() {
+			return name;
+		}
+	}
+	
+	public class Room1 extends Room {
+		String name = "Storage Closet";
+		roomDescription = "You look around an empty room. There are not windows or doors, you are trapped here forever.";
+		connectingRooms = ;
+		Item batteries = true;
+	}
+	
+	public class Room2 extends Room {
+		String name = "Young Adults Section";
+		public String setName(){
+			return name;
+		}
 		
 	}
+	
+	public class Room3 extends Room {
+		String name = "Archives";
+	}
+	
+	public class Room4 extends Room {
+		String name = "Elevator";
+	}
+	
+	public class Room5 extends Room {
+		String name = "Lobby";
+	}
+	
+	public class Room6 extends Room {
 		
+	}
+	
 // put descriptions into text files so that it is easier to change them
 
-}
