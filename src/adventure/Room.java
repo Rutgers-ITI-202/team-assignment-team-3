05b2947da6b338ada75400175cd6586e6a67db06
LaurenRichardson <lnr38@scarletmail.rutgers.edu>
2017-04-21 17:25:08 -0400

    Add files via upload

--- src/adventure/Room.java
+++ src/adventure/Room.java
@@ -1,47 +1,119 @@
 package adventure;
 
 public class Room {
-	boolean hasitem = true;
+		
+	public Room() {
+		private Boolean location;
+		private Boolean item;
+		private String itemName;
+		static String itemDescription;
+		String description;
+		String altDescription;	
+	}
 	
 	public class YA extends Room {
-		String IdentificationCard = "Name: Jamie Lannister \nDate of Birth: April 01, 1995"
+		location = false;
+		item = true;
+		itemName = "IdentificationCard";
+		itemDescription = "Name: Jamie Lannister \nDate of Birth: April 01, 1995"
 				+ "\nEye Color: Oh My~ \nSex: Unknown";
+		description = "";
+		altDescription = "";
 		String Phone = "Model: iPhone 7 (dead batteries)";
 	}
-	public class Bathroom extends Room {
-		String Flashlight = "Nicknamed: Lightsaber.(no batteries)";
-	}
+	
 	public class Storage extends Room {
-		String Batteries = "Flashlight Batteries. Use to turn on Flashlight (Lightsaber)";
+		location = false;
+		item = true;
+		itemName = "Batteries";
+		itemDescription = "Flashlight Batteries. Use to turn on Flashlight (Lightsaber)";
+		description = "";
+		altDescription = "";
 	}
-	public class Comic extends Room {
-		String BabyRoboticDragon = "Dragons are cool, babies are dumb, if you see a puppy, you better run!";
-		boolean Book = false;
-	}
-	public class Office extends Room {
-		String Key = "The key to opening the Media Room!";
-	}
-	public class Media extends Room {
-		String MasterKey = " You'll need this to swipe open the exit door in the Lobby";
-	}
-	public class Children extends Room {
-		String Wallet = " You'll need this to pay for the Uber ride back to Campus";
-	}
-	public class Lobby extends Room{
-		boolean Lock = true;
-	}
-	public class Archives extends Room{
-		String Map = "Second Floor:   |Media Room|   <-->   |Comics Section|       <-->    |Librarian's Office|\n"
+	
+	public class Archives extends Room {
+		location = false;
+		item = true;
+		itemName = "Map";
+		itemDescription = "Second Floor:   |Media Room|   <-->   |Comics Section|       <-->    |Librarian's Office|\n"
 				+ "\nFirst Floor:    |Bathroom|     <-->   |Children's Section|   <-->    |Lobby| \n"
 				+ "\nBasement:       |Storage|      <-->   |Young Adults Section| <-->    |Archives|";
+		description = "";
+		altDescription = "";
 	}
-	public class Hidden_Dungeon extends Room{
-		
+	
+	public class Elevator extends Room {
+		location = false;
+		item = false;
+		description = "";
 	}
-	public class HydraHeadquarters extends Room{
-		
+	
+	public class Lobby extends Room {
+		location = false;
+		description = "";
+		boolean Lock = true;
+	}
+	
+	public class Children extends Room {
+		location = false;
+		item = true;
+		itemName = "Wallet";
+		itemDescription = "You'll need this to pay for the Uber ride back to Campus";
+		description = "";
+		altDescription = "";
+	}
+	
+	public class Bathroom extends Room {
+		location = false;
+		item = true;
+		itemName = "Flashlight";
+		itemDescription = "Nicknamed: Lightsaber.(no batteries)";
+		description = "";
+		altDescription = "";
+	}
+	
+	public class Office extends Room {
+		location = false;
+		item = true;
+		itemName = "Key";
+		itemDescription = "The key to opening the Media Room!";
+		description = "";
+		altDescription = "";
+	}
+	
+	public class Comic extends Room {
+		location = false;
+		item = true;
+		itemName = "Book";
+		itemDescription = "";
+		description = "";
+		altDescription = "";
+	}
+
+	public class Media extends Room {
+		location = false;
+		item = true;
+		itemName = "MasterKey";
+		itemDescription = " You'll need this to swipe open the exit door in the Lobby";
+		description = "";
+		altDescription = "";
+	}
+	
+	public class Hidden_Dungeon extends Room {
+		location = false;
+		item = true;
+		itemName = "BabyRoboticDragon";
+		itemDescription = "Dragons are cool, babies are dumb, if you see a puppy, you better run!";
+		description = "";
+		altDescription = "";
+	}
+	
+	public class StormTrooperRoom extends Room {
+		location = false;
+		item = true;
+		itemName = "";
+		itemDescription = "";
+		description = "";
+		altDescription = "";
 	}
 }
-	
-// put descriptions into text files so that it is easier to change them
-
