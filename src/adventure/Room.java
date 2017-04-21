e092afe4a7cd8cc66438887e92940c77799aaec6
LaurenRichardson <lnr38@scarletmail.rutgers.edu>
2017-04-21 17:23:32 -0400

    Add files via upload

--- src/adventure/Room.java
+++ src/adventure/Room.java
@@ -3,28 +3,28 @@ package adventure;
 public class Room {
 	boolean hasitem = true;
 	
-	public class YA extends Room{
+	public class YA extends Room {
 		String IdentificationCard = "Name: Jamie Lannister \nDate of Birth: April 01, 1995"
 				+ "\nEye Color: Oh My~ \nSex: Unknown";
 		String Phone = "Model: iPhone 7 (dead batteries)";
 	}
-	public class Bathroom extends Room{
+	public class Bathroom extends Room {
 		String Flashlight = "Nicknamed: Lightsaber.(no batteries)";
 	}
-	public class Storage extends Room{
+	public class Storage extends Room {
 		String Batteries = "Flashlight Batteries. Use to turn on Flashlight (Lightsaber)";
 	}
-	public class Comic extends Room{
+	public class Comic extends Room {
 		String BabyRoboticDragon = "Dragons are cool, babies are dumb, if you see a puppy, you better run!";
 		boolean Book = false;
 	}
-	public class Office extends Room{
+	public class Office extends Room {
 		String Key = "The key to opening the Media Room!";
 	}
-	public class Media extends Room{
+	public class Media extends Room {
 		String MasterKey = " You'll need this to swipe open the exit door in the Lobby";
 	}
-	public class Children extends Room{
+	public class Children extends Room {
 		String Wallet = " You'll need this to pay for the Uber ride back to Campus";
 	}
 	public class Lobby extends Room{
