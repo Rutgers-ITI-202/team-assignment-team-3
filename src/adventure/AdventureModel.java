9df43b549278b773d4e67055fe423d911955fdb1
koalathreat <edwin.zheng@rutgers.edu>
2017-04-26 22:59:20 -0400

    Update AdventureModel.java

--- src/adventure/AdventureModel.java
+++ src/adventure/AdventureModel.java
@@ -8,31 +8,25 @@ This class:
 	o	As commands are entered, they should be copied to the main text area.
 	o	The method that executes commands should return a String to be displayed in the main text area.
 */
-package adventure;
-
+import java.util.ArrayList;
 import java.util.Scanner;
 import java.io.File;
 import java.io.FileNotFoundException;
 
+
 public class AdventureModel {
-	//private static Adventurer student = new Adventurer();
+	private static Adventurer student = new Adventurer();
+	private static ArrayList<String> textfile = new ArrayList<String>();
 	
-	public void beginning(){
-		File inputFile = new File("Intro.txt");
-		try{
-			String input;
-			Scanner in = new Scanner(inputFile);
-			while (in.hasNextLine()){
-				input = in.nextLine();
-				System.out.println(input);
-			}
-			in.close();
-		}
-		catch(FileNotFoundException e){
-			e.printStackTrace();
-		}
+	private static ArrayList<String> getStorage() {
+		return textfile;
 	}
-		public void datadump(){
+
+	private static void setStorage(ArrayList<String> storage) {
+		textfile = storage;
+	}
+	
+	public void datadump(){
 		File inputFile = new File("Descriptions.txt");
 		try{
 			int counter = 0;
@@ -49,27 +43,41 @@ public class AdventureModel {
 			e.printStackTrace();
 		}
 	}
+	public void beginning(){
+		int num = 0;
+		while(num<=8){
+			System.out.println(textfile.get(num));
+			num++;
+		}
+	}
 	
 	public void end(){
-		System.out.println("Any progress will be lost. Bye~");
+		System.out.println(textfile.get(10));
 		System.exit(0);
 	}
 	
 	public void look(){
-		room = 
-		System.out.println()
+		
 	}
 	
 	public void pickup(){
-		
+
 	}
 	
 	public void drop(){
-		
+
 	}
 	
 	public void openbag(){
-		
+
+	}
+	
+	public void move(){
+
+	}
+	
+	public void map(){
+
 	}
 }
 
