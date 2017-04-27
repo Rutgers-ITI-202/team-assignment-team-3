2b4b3793c1951fca6bec902d2e37b37294adefc4
koalathreat <edwin.zheng@rutgers.edu>
2017-04-26 22:56:58 -0400

    Update TextAdventure.java

--- src/adventure/TextAdventure.java
+++ src/adventure/TextAdventure.java
@@ -18,10 +18,11 @@ public class TextAdventure {
 		System.out.println("\nAvailable Commands: [Start Game] [Exit Game]");
 		
 		while(condition == false){
-			try {
+			try{
 				String input = myScanner.nextLine();
 				if(input.equalsIgnoreCase("Start Game")){
 					condition = true;
+					JaimeLannister.datadump();
 					JaimeLannister.beginning();
 				}
 
@@ -30,7 +31,7 @@ public class TextAdventure {
 					condition = true;
 				}
 				
-				else {
+				else{
 					condition = false;
 					System.out.println("Sorry, that was an invalid input");
 				}
@@ -42,48 +43,4 @@ public class TextAdventure {
 			}
 		}
 	}
-<<<<<<< Updated upstream
-=======
-	public static void commands(){
-		boolean condition = false;
-		myScanner = new Scanner(System.in);
-		System.out.println("Available Commands: \n1. = Look Around \n2. = Pick Up Item \n3. = Drop Item "
-				+ "\n4. = Look Inside Bookbag \n5. = Exit Game");
-		while(condition == false){
-			try{
-				String input = myScanner.nextLine();
-				int num = Integer.parseInt(input);
-				if(num == 1){
-					condition = true;
-					JaimeLannister.look();
-				}
-				else if(num == 2){
-					JaimeLannister.pickup();
-					condition = true;
-				}
-				else if(num == 3){
-					JaimeLannister.drop();
-					condition = true;
-				}
-				else if(num == 4){
-					JaimeLannister.openbag();
-					condition = true;
-				}
-				else if(num == 5){
-					JaimeLannister.end();
-					condition = true;
-				}
-				else{
-					condition = false;
-					System.out.println("Sorry, that was an invalid input");
-				}
-			}
-			
-			catch(Exception e){
-				System.out.println("Sorry, that was an invalid input");
-				condition = false;
-			}
-		}	
-	}
->>>>>>> Stashed changes
 }
