5ddb2dcee6c85d16b0eded004b30a32cd3f83fff
LaurenRichardson <lnr38@scarletmail.rutgers.edu>
2017-04-26 23:13:35 -0400

    Rooms Update
    Made all the rooms into objects

--- src/adventure/Room.java
+++ src/adventure/Room.java
@@ -1,122 +1,39 @@
 package adventure;
 
-public class Room {
-<<<<<<< HEAD
-=======
-		
-	public Room() {
->>>>>>> origin/master
-		private Boolean location;
-		private Boolean item;
-		private String itemName;
-		static String itemDescription;
-		String description;
-		String altDescription;	
-	}
-	
-	public class YA extends Room {
-		location = false;
-		item = true;
-		itemName = "IdentificationCard";
-		itemDescription = "Name: Jamie Lannister \nDate of Birth: April 01, 1995"
-				+ "\nEye Color: Oh My~ \nSex: Unknown";
-		description = "";
-		altDescription = "";
-		String Phone = "Model: iPhone 7 (dead batteries)";
-	}
-	
-	public class Storage extends Room {
-		location = false;
-		item = true;
-		itemName = "Batteries";
-		itemDescription = "Flashlight Batteries. Use to turn on Flashlight (Lightsaber)";
-		description = "";
-		altDescription = "";
-	}
-	
-	public class Archives extends Room {
-		location = false;
-		item = true;
-		itemName = "Map";
-		itemDescription = "Second Floor:   |Media Room|   <-->   |Comics Section|       <-->    |Librarian's Office|\n"
-				+ "\nFirst Floor:    |Bathroom|     <-->   |Children's Section|   <-->    |Lobby| \n"
-				+ "\nBasement:       |Storage|      <-->   |Young Adults Section| <-->    |Archives|";
-		description = "";
-		altDescription = "";
-	}
-	
-	public class Elevator extends Room {
-		location = false;
-		item = false;
-		description = "";
-	}
-	
-	public class Lobby extends Room {
-		location = false;
-		description = "";
-		boolean Lock = true;
-	}
-	
-	public class Children extends Room {
-		location = false;
-		item = true;
-		itemName = "Wallet";
-		itemDescription = "You'll need this to pay for the Uber ride back to Campus";
-		description = "";
-		altDescription = "";
-	}
-	
-	public class Bathroom extends Room {
-		location = false;
-		item = true;
-		itemName = "Flashlight";
-		itemDescription = "Nicknamed: Lightsaber.(no batteries)";
-		description = "";
-		altDescription = "";
-	}
-	
-	public class Office extends Room {
-		location = false;
-		item = true;
-		itemName = "Key";
-		itemDescription = "The key to opening the Media Room!";
-		description = "";
-		altDescription = "";
-	}
-	
-	public class Comic extends Room {
-		location = false;
-		item = true;
-		itemName = "Book";
-		itemDescription = "";
-		description = "";
-		altDescription = "";
-	}
+import java.util.ArrayList;
 
-	public class Media extends Room {
-		location = false;
-		item = true;
-		itemName = "MasterKey";
-		itemDescription = " You'll need this to swipe open the exit door in the Lobby";
-		description = "";
-		altDescription = "";
+public class Room {
+		
+	static ArrayList <Room> roomList = new ArrayList<Room>();
+	
+	public Room (Boolean x, Boolean y, String z){
+		Boolean location = x;
+		Boolean item = y;
+		String itemName = z;
 	}
 	
-	public class Hidden_Dungeon extends Room {
-		location = false;
-		item = true;
-		itemName = "BabyRoboticDragon";
-		itemDescription = "Dragons are cool, babies are dumb, if you see a puppy, you better run!";
-		description = "";
-		altDescription = "";
-	}
-	
-	public class StormTrooperRoom extends Room {
-		location = false;
-		item = true;
-		itemName = "";
-		itemDescription = "";
-		description = "";
-		altDescription = "";
-	}
+	class addRooms {{
+		Room YA = new Room(false, true, "Identification Card");
+		roomList.add(YA);
+		Room Storage = new Room(false, true, "Batteries");
+		roomList.add(Storage);
+		Room Archives = new Room(false, true, "Map");
+		roomList.add(Archives);
+		Room Elevator = new Room(false, false, "");
+		roomList.add(Elevator);
+		Room Lobby = new Room(false, false, "");
+		roomList.add(Lobby);
+		Room Children = new Room(false, true, "Wallet");
+		roomList.add(Children);
+		Room Bathroom = new Room(false, true, "Flashlight");
+		roomList.add(Bathroom);
+		Room Office = new Room(false, true, "Key");
+		roomList.add(Office);
+		Room Comic = new Room(false, true, "Book");
+		roomList.add(Comic);
+		Room Media = new Room(false, true, "Master Key");
+		roomList.add(Media);
+		Room Hidden_Dungeon = new Room(false, true, "Baby Robotic Dragon");
+		roomList.add(Hidden_Dungeon);
+	}}
 }
---
+++ src/adventure/SuperRoom.java
@@ -0,0 +1,5 @@
+package adventure;
+
+public class SuperRoom {
+
+}
