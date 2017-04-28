125261ab271a9a87b407b0673c3d8ba49ef76e22
LaurenRichardson <lnr38@scarletmail.rutgers.edu>
2017-04-27 21:32:26 -0400

    Room Classes
    here is what I tried to do before

--- src/adventure/Room.java
+++ src/adventure/Room.java
@@ -1,39 +1,124 @@
 package adventure;
 
-import java.util.ArrayList;
+import adventure.Room.SuperRoom;
 
 public class Room {
-		
-	static ArrayList <Room> roomList = new ArrayList<Room>();
 	
-	public Room (Boolean x, Boolean y, String z){
-		Boolean location = x;
-		Boolean item = y;
-		String itemName = z;
+	class SuperRoom {
+		Boolean location;
+		Boolean item;
+		String itemName;
+		String itemDescription;
+		String description;
+		String altDescription;
+		public String getName() {
+			return itemName;
+		}
 	}
 	
-	class addRooms {{
-		Room YA = new Room(false, true, "Identification Card");
-		roomList.add(YA);
-		Room Storage = new Room(false, true, "Batteries");
-		roomList.add(Storage);
-		Room Archives = new Room(false, true, "Map");
-		roomList.add(Archives);
-		Room Elevator = new Room(false, false, "");
-		roomList.add(Elevator);
-		Room Lobby = new Room(false, false, "");
-		roomList.add(Lobby);
-		Room Children = new Room(false, true, "Wallet");
-		roomList.add(Children);
-		Room Bathroom = new Room(false, true, "Flashlight");
-		roomList.add(Bathroom);
-		Room Office = new Room(false, true, "Key");
-		roomList.add(Office);
-		Room Comic = new Room(false, true, "Book");
-		roomList.add(Comic);
-		Room Media = new Room(false, true, "Master Key");
-		roomList.add(Media);
-		Room Hidden_Dungeon = new Room(false, true, "Baby Robotic Dragon");
-		roomList.add(Hidden_Dungeon);
+	class YA extends SuperRoom {{
+		location = false;
+		item = true;
+		itemName = "Identification Card";
+		itemDescription = "Name: Jamie Lannister \nDate of Birth: April 01, 1995"
+				+ "\nEye Color: Oh My~ \nSex: Unknown";
+		description = "";
+		altDescription = "";
+		String Phone = "Model: iPhone 7 (dead batteries)";
+	}}
+	
+	class Storage extends SuperRoom {{
+		location = false;
+		item = true;
+		itemName = "Batteries";
+		itemDescription = "Flashlight Batteries. Use to turn on Flashlight (Lightsaber)";
+		description = "";
+		altDescription = "";
+	}}
+	
+	public class Archives extends SuperRoom {{
+		location = false;
+		item = true;
+		itemName = "Map";
+		itemDescription = "Second Floor:   |Media Room|   <-->   |Comics Section|       <-->    |Librarian's Office|\n"
+				+ "\nFirst Floor:    |Bathroom|     <-->   |Children's Section|   <-->    |Lobby| \n"
+				+ "\nBasement:       |Storage|      <-->   |Young Adults Section| <-->    |Archives|";
+		description = "";
+		altDescription = "";
+	}}
+	
+	public class Elevator extends SuperRoom {{
+		location = false;
+		item = false;
+		description = "";
+	}}
+	
+	public class Lobby extends SuperRoom {{
+		location = false;
+		description = "";
+		boolean Lock = true;
+	}}
+	
+	public class Children extends SuperRoom {{
+		location = false;
+		item = true;
+		itemName = "Wallet";
+		itemDescription = "You'll need this to pay for the Uber ride back to Campus";
+		description = "";
+		altDescription = "";
+	}}
+	
+	public class Bathroom extends SuperRoom {{
+		location = false;
+		item = true;
+		itemName = "Flashlight";
+		itemDescription = "Nicknamed: Lightsaber.(no batteries)";
+		description = "";
+		altDescription = "";
+	}}
+	
+	public class Office extends SuperRoom {{
+		location = false;
+		item = true;
+		itemName = "Key";
+		itemDescription = "The key to opening the Media Room!";
+		description = "";
+		altDescription = "";
+	}}
+	
+	public class Comic extends SuperRoom {{
+		location = false;
+		item = true;
+		itemName = "Book";
+		itemDescription = "";
+		description = "";
+		altDescription = "";
+	}}
+
+	public class Media extends SuperRoom {{
+		location = false;
+		item = true;
+		itemName = "MasterKey";
+		itemDescription = " You'll need this to swipe open the exit door in the Lobby";
+		description = "";
+		altDescription = "";
+	}}
+	
+	public class Hidden_Dungeon extends SuperRoom {{
+		location = false;
+		item = true;
+		itemName = "BabyRoboticDragon";
+		itemDescription = "Dragons are cool, babies are dumb, if you see a puppy, you better run!";
+		description = "";
+		altDescription = "";
+	}}
+	
+	public class StormTrooperRoom extends SuperRoom {{
+		location = false;
+		item = true;
+		itemName = "";
+		itemDescription = "";
+		description = "";
+		altDescription = "";
 	}}
 }
