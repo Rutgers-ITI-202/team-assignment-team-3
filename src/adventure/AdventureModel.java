7ced072b90ee275588225c8d8142b188b5598160
koalathreat <edwin.zheng@rutgers.edu>
2017-04-28 17:25:28 -0400

    Update AdventureModel.java

--- src/adventure/AdventureModel.java
+++ src/adventure/AdventureModel.java
@@ -26,10 +26,17 @@ public class AdventureModel {
 	private Room currentroom;
 	private Scanner myScanner = new Scanner(System.in);
 	private static Adventurer student = new Adventurer();
-	
+	/**
+	*Creates construtor for rooms. Creates Scanner and the student that connects this class to Adventurer.
+	*/
 	public ArrayList<String> getTextfiles() {
 		return student.getTextfile();
+	/**
+	*This gets the appropriate string descriptions from our text file which we imported in Adventurer
+	*@returns string from textfile
+	*/
 	}
+	
 	public void beginning(){
 		int num = 0;
 		while(num<=8){
@@ -38,18 +45,28 @@ public class AdventureModel {
 		}
 		currentroom = new Room2();
 		command();
+	/**
+	* This just reads the first 9 lines of intro text from my textfile ArrayList which holds all the descriptions for this game
+	* Then it sets currentroom to the starting point of the game (YA Section). Basically places player in a room
+	*/
 	}
 	
 	public void win(){
 		System.out.println("Congratulations, you have successfully escaped the library!");
-		if (student.getInventory()[1] && student.getInventory()[3] && student.getInventory()[4] && student.getInventory()[7])
+		if (student.getInventory()[1] && student.getInventory()[3] && student.getInventory()[4] && student.getInventory()[9])
 			System.out.println("You were smart to collect all those textbooks, you aslo managed to get an A+ on your exam!");
 		end();
+	/**
+	* This is the method that runs when you win the game. If you have 4 of the required items, it also prints that you pass your exam.
+	*/
 	}
 	
 	public void end(){
 		System.out.println(student.getTextfile().get(10));
 		System.exit(0);
+		/**
+		*This just prints the bye bye statement. Then kills the program.
+		*/
 	}
 	
 	public void map(){
@@ -62,17 +79,27 @@ public class AdventureModel {
 		else{
 			System.out.println(student.getTextfile().get(50)+ "\n");
 		}
+		/**
+		*This prints out the map for the user so that the player doesn't get lost.
+		*Also has an else statement that runs if you don't have the map.
+		*/
 	}
 	
 	public void command(){
 		while(true){
 			System.out.println(student.getTextfile().get(59)+ "\n");
 			String input = myScanner.nextLine();
+			/**
+			*Just a loop that makes sure the player always have a command display for as long as they're playing.
+			*Also takes in the user input
+			*/
 			switch(input.toLowerCase()){
 				case "look":
 					System.out.println(currentroom.look(student.getTextfile()) + "\n");
 					break;
-					
+					/**
+					*This just calls the appropriate room subclass to get the appropriate description lines
+					*/
 				case "pick up":
 					if (student.getInventory()[currentroom.pickup()] == true)
 						System.out.println("There is nothing in here to pick up."+ "\n");
@@ -83,20 +110,33 @@ public class AdventureModel {
 								System.out.println(student.getTextfile().get(37)+ "\n");
 							}//surround with try/catch - google exception outofbounds error
 					break;
+					/**
+					*Checks for whether you already have item. If you don't have item,
+					*It prints you took it. Room8 is special case so it prints something extra for it.
+					*/
 			
 				case "drop":
 					//surround with try/catch
 					student.drop();
 					break;
+					/**
+					*Calls drop method to remove item
+					*/
 			
 				case "open bag":
 					student.Inventoryprint();
 					System.out.println();
 					break;
+					/**
+					*Calls a method in Adventurer to print out the inventory
+					*/
 			
 				case "end":
 					end();
 					break;
+					/**
+					*Calls a method in this class that kills the program.
+					*/
 			
 				case "move right":
 					Room i = currentroom.move(2);
@@ -163,10 +203,16 @@ public class AdventureModel {
 				case "map":
 					map();
 					break;
+					/**
+					*Calls map method to either print error or map
+					*/
 			
 				default:
 					System.out.println("Error: Bad input");
 					break;
+					/**
+					*Catches all the wrong command
+					*/
 			}
 		}
 	}
